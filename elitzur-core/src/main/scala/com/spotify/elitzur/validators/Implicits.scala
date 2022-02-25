f65c41aef44dbb975197c8db532f62526aeed88e
Jack Dingilian <jackd@spotify.com>
2022-02-25 15:43:35 -0500

    Push metrics reporting down into FieldValidator

--- elitzur-core/src/main/scala/com/spotify/elitzur/validators/Implicits.scala
+++ elitzur-core/src/main/scala/com/spotify/elitzur/validators/Implicits.scala
@@ -24,25 +24,25 @@ trait Implicits {
   //scalastyle:off line.size.limit
   import Validator._
 
-  implicit val stringValidator: FieldValidator[String] = new IgnoreValidator[String]
-  implicit val longValidator: FieldValidator[Long] = new IgnoreValidator[Long]
-  implicit val doubleValidator: FieldValidator[Double] = new IgnoreValidator[Double]
-  implicit val booleanValidator: FieldValidator[Boolean] = new IgnoreValidator[Boolean]
-  implicit val arrayByteValidator: FieldValidator[Array[Byte]] = new IgnoreValidator[Array[Byte]]
-  implicit val floatValidator: FieldValidator[Float] = new IgnoreValidator[Float]
-  implicit val intValidator: FieldValidator[Int] = new IgnoreValidator[Int]
+  implicit def stringValidator(implicit reporter: MetricsReporter): FieldValidator[String] = new IgnoreValidator[String]
+  implicit def longValidator(implicit reporter: MetricsReporter): FieldValidator[Long] = new IgnoreValidator[Long]
+  implicit def doubleValidator(implicit reporter: MetricsReporter): FieldValidator[Double] = new IgnoreValidator[Double]
+  implicit def booleanValidator(implicit reporter: MetricsReporter): FieldValidator[Boolean] = new IgnoreValidator[Boolean]
+  implicit def arrayByteValidator(implicit reporter: MetricsReporter): FieldValidator[Array[Byte]] = new IgnoreValidator[Array[Byte]]
+  implicit def floatValidator(implicit reporter: MetricsReporter): FieldValidator[Float] = new IgnoreValidator[Float]
+  implicit def intValidator(implicit reporter: MetricsReporter): FieldValidator[Int] = new IgnoreValidator[Int]
 
-  implicit def baseTypeValidator[T <: BaseValidationType[_]: ClassTag]: FieldValidator[T] = new BaseFieldValidator[T]
-  implicit def optionTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag]: FieldValidator[Option[T]] = new OptionTypeValidator[T]
-  implicit def statusTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag]: FieldValidator[ValidationStatus[T]] = new StatusTypeValidator[T]
-  implicit def statusOptionTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag]: FieldValidator[ValidationStatus[Option[T]]] = new StatusOptionTypeValidator[T]
+  implicit def baseTypeValidator[T <: BaseValidationType[_]: ClassTag](implicit reporter: MetricsReporter): FieldValidator[T] = new BaseFieldValidator[T]
+  implicit def optionTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag](implicit reporter: MetricsReporter): FieldValidator[Option[T]] = new OptionTypeValidator[T]
+  implicit def statusTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag](implicit reporter: MetricsReporter): FieldValidator[ValidationStatus[T]] = new StatusTypeValidator[T]
+  implicit def statusOptionTypeValidator[T <: BaseValidationType[_]: FieldValidator: ClassTag](implicit reporter: MetricsReporter): FieldValidator[ValidationStatus[Option[T]]] = new StatusOptionTypeValidator[T]
   implicit def wrappedValidator[T: Validator]: Validator[ValidationStatus[T]] = new WrappedValidator[T]
   implicit def optionValidator[T: Validator]: Validator[Option[T]] = new OptionValidator[T]
-  implicit def dynamicTypeValidator[T <: DynamicValidationType[_, _, _]: ClassTag]: DynamicValidator[T] = new DynamicValidator[T]
-  implicit def seqValidator[T: Validator: ClassTag](implicit reporter: MetricsReporter): Validator[Seq[T]] = wrapSeqLikeValidator(() => Seq.newBuilder[T])
-  implicit def listValidator[T: Validator: ClassTag](implicit reporter: MetricsReporter): Validator[List[T]] = wrapSeqLikeValidator(() => List.newBuilder[T])
-  implicit def arrayValidator[T: Validator: ClassTag](implicit reporter: MetricsReporter): Validator[Array[T]] = wrapSeqLikeValidator(() => Array.newBuilder[T])
-  implicit def vectorValidator[T: Validator: ClassTag](implicit reporter: MetricsReporter): Validator[Vector[T]] = wrapSeqLikeValidator(() => Vector.newBuilder[T])
+  implicit def dynamicTypeValidator[T <: DynamicValidationType[_, _, _]: ClassTag](implicit reporter: MetricsReporter): DynamicValidator[T] = new DynamicValidator[T]
+  implicit def seqValidator[T: Validator: ClassTag]: Validator[Seq[T]] = wrapSeqLikeValidator(() => Seq.newBuilder[T])
+  implicit def listValidator[T: Validator: ClassTag]: Validator[List[T]] = wrapSeqLikeValidator(() => List.newBuilder[T])
+  implicit def arrayValidator[T: Validator: ClassTag]: Validator[Array[T]] = wrapSeqLikeValidator(() => Array.newBuilder[T])
+  implicit def vectorValidator[T: Validator: ClassTag]: Validator[Vector[T]] = wrapSeqLikeValidator(() => Vector.newBuilder[T])
   //scalastyle:on line.size.limit
 
 }
